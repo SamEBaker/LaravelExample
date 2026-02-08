bd364e3b73d7c1d4acd05f11ce0d387347dba761
Samantha Esparza-Baker <123209839+SamEBaker@users.noreply.github.com>
2026-02-08 14:12:17 -0700

    Laravel cont
    It shows an extra name column in the user table and wont let me seed it???

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,35 +2,19 @@
 
 namespace App\Models;
 use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Job {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => 'a lot',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => 'eh',
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => 'NOT a lot',
-            ]
-        ];
+class Job extends Model {
+    use HasFactory;
+    protected $table = 'job_listings';
+
+    protected $fillable = ['title', 'salary'];
+
+    public function employer() {
+        return $this->belongsTo(Employer::class);
     }
-
-    public static function find(int $id): array
-    {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-
-        if(! $job){
-            abort(404);
-        }
+     public function tags() {
+        return $this->belongsToMany(Tag::class, foreignPivotKey: 'job_listing_id');
     }
 }
--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -2,13 +2,34 @@
     <x-slot:heading>
         Job Listings
     </x-slot:heading>
-    <ul>
+
+    <form method="GET" action="{{ url('/jobs') }}">
+        <label for="sort">Sort By:</label>
+
+        <select name="sort" id="sort" onchange="this.form.submit()">
+            <option value="id" {{ request('sort') === 'id' ? 'selected' : '' }}>
+                Job ID
+            </option>
+
+            <option value="title" {{ request('sort') === 'title' ? 'selected' : '' }}>
+                Job Title (A–Z)
+            </option>
+
+            <option value="salary" {{ request('sort') === 'salary' ? 'selected' : '' }}>
+                Salary (Lowest to Highest)
+            </option>
+        </select>
+    </form>
+
+    <div class="space-y-4">
         @foreach ($jobs as $job)
-            <li>
-                <a href='/jobs/'>
-                    <strong>{{ $job['title'] }}</strong>: Pays <strong>{{$job['salary']}}</strong> per year.
-                </a>
-            </li>
+            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border bg-indigo-200 border-indigo-950 rounded-lg">
+                <div class="font-bold text-blue-500 text-sm">{{ $job->employer->name }}</div>
+
+                <div>
+                    <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year
+                </div>
+            </a>
         @endforeach
-    </ul>
+    </div>
 </x-layout>
